370e2154cf102fb8b0d48544c7b57a4ca2913b7f
cancng <me@mahmutcan.work>
2021-04-06 23:20:40 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+class date {
+  char *p;
+
+ public:
+  date(char *s);
+  ~date() {
+    cout << "Destructing" << endl;
+    delete[] p;
+  }
+  char *get() { return p; }
+};
+
+date::date(char *s) {
+  int l;
+
+  l = strlen(s) + 1;
+  p = new char[l];
+  if (!p) {
+    cout << "Allocation error" << endl;
+    exit(1);
+  }
+  strcpy(p, s);
+}
+
+void show(date x) {
+  char *s;
+  s = x.get();
+  cout << s << endl;
+}
+
+int main() {
+  date sdate("04/06/21");
+  date xdate("00/00/00");
+
+  xdate = sdate;
+  show(sdate);
+  show(xdate);
+
+  cout << sdate.get() << " " << xdate.get() << endl;
+
+  return 0;
+}
